2c7b43881feb6dc3c20a5272de074303f7f2f829
lynnkoo <lynnkoo.1120@gmail.com>
2020-07-09 18:31:17 +0800

    update for cms package version

--- projects/cms/src/App.tsx
+++ projects/cms/src/App.tsx
@@ -1,28 +1,18 @@
 import * as React from 'react'
 import { Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import { configureStore, createLazyComponent } from 'redux-async-kit'
-import { PageRouter } from '@smoex-web/basic'
-import { userSlice } from '@smoex-business/user'
-
-const store = configureStore({
-  injector: userSlice.injector,
-})
-
-window['store'] = store
+import { PageContainer } from '@smoex-web/basic'
 
 const HomePage = createLazyComponent({
-  injector: userSlice.injector,
+  // injector: userSlice.injector,
   loader: () => import('./containers/HomePage' /* webpackChunkName: "home" */),
 })
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <PageRouter>
-        <Route exact path="/" component={HomePage} />
-      </PageRouter>
-    </Provider>
+    <PageContainer>
+      <Route exact path="/" component={HomePage} />
+    </PageContainer>
   )
 }
 
--- projects/cms/src/index.tsx
+++ projects/cms/src/index.tsx
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 // import VConsole from 'vconsole'
 import { initWindowWidth } from 'basic-kit-browser'
+import App from './App'
 initWindowWidth()
 
-const App = React.lazy(() => import('./App' /* webpackChunkName: "app" */))
+// const App = React.lazy(() => import('./App' /* webpackChunkName: "app" */))
 
-const LazyApp = () => {
-  return (
-    <Suspense fallback={null}>
-      <App />
-    </Suspense>
-  )
-}
-
-ReactDOM.render(<LazyApp />, document.getElementById('root'))
+ReactDOM.hydrate(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
